eaaf526301a048916e16272776d2eb0169dd8f88
Electogenius <69585636+Electogenius@users.noreply.github.com>
2021-12-13 23:10:25 +0530

    this=A(MESS)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ledit
 Buggy code editor in thy browser
+
+TODO: Use domparser to change source of script/link tags
--- ledit.js
+++ ledit.js
@@ -2,71 +2,111 @@
 var s = document.createElement("style")
 s.innerHTML = `
 div.leditthing{
-	color: black;
+	color: white;
 	display: block;
 	position: relative;
 	overflow: scroll;
 	white-space: nowrap;
+	border-radius: 5px;
+	background-color: #111;
+	padding: 0px
 }
-textarea.leditthing{
-	opacity: 0.1;
-	color: red;
+textareakinda.leditthing{
+	display: block;
+	opacity: 0.11;
+	color: rgba(0,0,0,0);
+	color: #00f;
 	z-index: 10;
-	width: 98vw;
+	width: 95vw;
 	white-space: nowrap;
-	color: white;
+	padding: 0px;
+	caret-color: white;
+	overflow: auto;
+	outline: none;
+	background: none;
+	font-family: monospace;
+	user-select: none;
+}
+.function{
+	color: #07f
+}
+.basic-string{
+	color: #af3
+}
+.basic-number{
+	color: red
+}
+.math{
+	color: #f50
+}
+span.tab{
+	width: 300px;
+}
+span.tab:after{
+	content: "aaa"
 }
 `
 document.head.appendChild(s)
-class Leditor {
+var Ledit = {}
+Ledit.or = class {
 	constructor(obj) {
 		var defaults = {
-			font: "menlo, monospace",
+			font: "monospace",
 			source: document.body,
-			value: "{}",
-			fontSize: "15px"
+			value: "",
+			fontSize: "15px",
+			locked: false,
+			language:"text"
 		}
 		for (var prop in obj) {
-			defaults[prop]=obj[prop]
+			defaults[prop] = obj[prop]
 		}
 		for (var prop in defaults) {
-			this[prop]=defaults[prop]
+			this[prop] = defaults[prop]
 		}
 	}
-	start(){
-		var ta = document.createElement("textarea")
+	start() {
+		var ta = document.createElement("textareakinda")
 		var ol = document.createElement("div")
 		ol.classList.add("leditthing")
 		ta.classList.add("leditthing")
-		ta.value = this.value
-		ol.innerText = this.value
+		ta.innerText = this.value
+		this.textarea = ta
+		this.overlay = ol
 		this.source.appendChild(ta)
 		this.source.appendChild(ol)
 		ta.style.position = "absolute"
 		ta.style.display = "block"
 		ta.style.fontFamily = this.font
 		ol.style.fontFamily = this.font
-		ta.style.padding = "0px"
+		//ta.style.padding = "0px"
 		ta.style.fontSize = this.fontSize
 		ol.style.fontSize = this.fontSize
-		ta.style.border = "none"
+		//ta.style.border = "none"
 		ol.style.marginTop = "2px"
 		ol.style.marginLeft = "2px"
 		ta.style.height = this.height
 		ol.style.height = this.height
-		ta.oninput = ()=>{
-			ol.innerText = event.target.value
+		ta.autocapitalize = "off"
+		ta.autocorrect = "off"
+		ta.autocomplete = "off"
+		ta.spellcheck = false
+		ta.contentEditable = !this.locked
+		//cursor
+		var cursor = document.createElement("cursor")
+		var latested = ta.innerText
+		var addition = ""
+		ta.oninput = (ev) => {
+			Rainbow.color(ta.innerText, this.language, function(c) {
+				ol.innerHTML = c.replace(/\n/g, "<br>")
+			})
 		}
-		ta.onscroll = ()=>{
+		ta.onscroll = () => {
 			ol.scrollTop = event.target.scrollTop
 			ol.scrollLeft = event.target.scrollLeft
 		}
+		Rainbow.color(ta.innerText, this.language, function(c) {
+			ol.innerHTML = c.replace(/\n/g, "<br>").replace(/\t/g, "<span class=tab>h</span>")
+		})
 	}
 }
-//test
-var ee = new Leditor({
-	source: document.getElementById("demo"),
-	value: "hello world",
-	height: "100px",
-})
-ee.start()
